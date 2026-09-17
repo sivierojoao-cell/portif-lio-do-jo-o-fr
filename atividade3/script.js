8248673514be4fbbd6dc470880b2cdfdfb62c38e
sivierojoao-cell <siviero.joao@escola.pr.gov.br>
2026-09-17 09:42:26 -0300

    Add files via upload

--- atividade3/script.js
+++ atividade3/script.js
@@ -1,1 +1,12 @@
-alert("FIM");
+let nota1trim;
+let nota2trim;
+let nota3trim;
+
+function calcular(){
+    nota1trim = Number(prompt("Digite a nota do primeiro trimestre:"));
+    nota2trim = Number(prompt("Digite a nota do segundo trimestre:"));
+
+    resultado = 180 - nota1trim + nota2trim;
+
+    
+}
